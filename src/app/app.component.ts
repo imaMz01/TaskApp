ccf9b16b2795ef0d77567ac14f0852752be29c59
imaMz01 <imanemzaouir66@gmail.com>
2024-08-22 12:54:20 +0100

    La gestion d'une liste de taches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './Model/Task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'AngularTp1';
+  i:number=1
+  tasks:Task[]=[{id:this.i++,description:"Définir les exigences fonctionnelles",completed:false,dateAjout:new Date()},
+    {id:this.i++,description:"Définir les exigences non fonctionnelles",completed:false,dateAjout:new Date()},
+    {id:this.i++,description:"Concevoir la solution",completed:false,dateAjout:new Date()},
+    {id:this.i++,description:"Réaliser la solution",completed:false,dateAjout:new Date()},
+    {id:this.i++,description:"Tester la solution",completed:false,dateAjout:new Date()}
+  ]
+  description:string=""
+  terminer(task:Task){
+    task.completed=true
+  }
+  ajouter(){
+    console.log(this.description)
+    this.tasks.push({id:this.i++,description:this.description,completed:false,dateAjout:new Date()})
+    this.description=""
+  }
 }
